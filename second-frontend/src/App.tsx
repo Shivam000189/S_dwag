6a55186baf107ac8313a85c62ef0e5fc2b6f3d04
Shivam000189 <shivamsharmass9897@gmail.com>
2026-03-17 21:05:57 +0530

    CreateComponent

--- second-frontend/src/App.tsx
+++ second-frontend/src/App.tsx
@@ -1,16 +1,16 @@
 
+import { useState } from 'react'
 import './App.css'
-import CreateComponet from './components/ui/CreateComponet'
+import { CreateComponent } from './components/ui/CreateComponet'
 import { MainPage } from './components/ui/MainPage'
 import { SideBar } from './components/ui/SideBar'
 
 function App() {
-  
 
   return (
     <div className='flex min-h-screen bg-brand-bg font-sans'>
         <SideBar></SideBar>
-        <CreateComponet open={true}/>
+        <CreateComponent open={true}/>
         <MainPage></MainPage>
     </div>
   )
--- second-frontend/src/components/ui/CreateComponet.tsx
+++ second-frontend/src/components/ui/CreateComponet.tsx
@@ -1,15 +1,42 @@
 import { useState } from "react"
+import { CrosIcon } from "../../icons/CrosIcon"
 
 
-export default function CreateComponet({open}) {
+export const CreateComponent = ({open}) => {
     
+    const handleClick = () => {
+        
+    }
+
     return <div>
         <div className={`fixed inset-0 flex justify-center items-center z-50 transition-all duration-300 ${open ? "bg-black/40 backdrop-blur-sm opacity-100" : "bg-transparent backdrop-blur-0 opacity-0"}`}>
-                <div className="flex">
+                <div className="h-100 w-100 bg-white justify-center rounded-lg shadow">
+                    <div className="flex justify-end p-2 cursor-pointer" onClick={handleClick}><CrosIcon /></div>
+                    <div className="flex justify-center">
+                        <div className="text-2xl font-medium">Create Content</div>
+                    </div>
 
+                    <div className="p-5">
+                        <Input name={"Link"} />
+                        <Input name={"Tink"} />
+                        <Input name={"Tags"} />
+                    </div>
+                    <div className="flex justify-center">
+                        <span className="h-10 w-50 text-3xl font-medium flex justify-center border rounded cursor-pointer hover:bg-amber-200">Sumbit</span>
+                    </div>
+                    
                 </div>
             
             </div>
 
+    </div>
+}
+
+
+
+function Input({name}){
+    return <div className="pt-5">
+        <span className="text-2xl font-medium pr-10">{name}</span>
+        <input type={"text"} className="h-10 w-64 border rounded-lg" />
     </div>
 }
